b5babc9e6df485763b514095fcfae1b20fab3bf6
lyons811 <lyonws@hawaii.edu>
2022-03-13 13:14:54 -1000

    Initial commit. Added member variables in class

--- Animal.cpp
+++ Animal.cpp
@@ -2,15 +2,13 @@
 ///         University of Hawaii, College of Engineering
 /// @brief  ee205_lab08c_all_creatures_great_and_small - EE 205 - Spr 2022
 ///
-/// @file main.cpp
+/// @file Animal.cpp
 /// @version 1.0
 ///
 /// @author Lyon Singleton <lyonws@hawaii.edu>
 /// @date   13_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
-#include <iostream>
+#include "Animal.h"
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+const string Animal::kingdom = "Animalia";
